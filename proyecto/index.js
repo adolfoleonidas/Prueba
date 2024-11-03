0cec0622055b65d48eab49e2733e3882eccab86c
adolfoleonidas <50957867+adolfoleonidas@users.noreply.github.com>
2024-11-03 09:17:02 -0500

    Delete proyecto directory

--- proyecto/index.js
+++
@@ -1,9 +0,0 @@
-import { JobsApp } from './js/app.js';
-
-console.log('Index.js cargado');
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOM cargado');
-    const app = new JobsApp();
-    app.init();
-});
